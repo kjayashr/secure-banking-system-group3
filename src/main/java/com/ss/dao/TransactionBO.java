5585459b191c3fc3d45af9cf9c38282ce5c6e630
Aurgho Bhattacharjee <aurghob@gmail.com>
2017-10-21 23:25:07 -0700

    Adding approve and decline functionality for tier1 non critical transactions without debit or credit

--- src/main/java/com/ss/dao/TransactionBO.java
+++ src/main/java/com/ss/dao/TransactionBO.java
@@ -4,9 +4,12 @@ import com.ss.model.TransactionDO;
 import java.util.List;
 
 public interface TransactionBO {
-	
-    public List<TransactionDO> getUnapprovedTransactionInfo(String userRole);
-	public boolean approveTransaction(int transactionId, String approverUserId, String userLevel);
-	public void declineTransaction(int transactionId);
+
+	List<TransactionDO> getUnapprovedTransactionInfo(String userRole);
+	boolean approveTransaction(int transactionId, String approverUserId, String userLevel);
+	void declineTransaction(int transactionId);
+    List<TransactionDO> getUnapprovedNonCriticalTransactions(String userRole);
+	boolean approveTransaction(int transactionId, String approverUserId);
+	boolean declineTransaction(int transactionId, String approverUserId);
 	    
 }
--- src/main/java/com/ss/daoImpl/TransactionBOImpl.java
+++ src/main/java/com/ss/daoImpl/TransactionBOImpl.java
@@ -15,8 +15,8 @@ import com.ss.model.TransactionDO;
 public class TransactionBOImpl implements TransactionBO {
 private static final String USER_ROLE_TIER1 = "TIER1";
 
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 	
     @Override
     public List<TransactionDO> getUnapprovedTransactionInfo(String userRole) {
@@ -27,19 +27,6 @@ private static final String USER_ROLE_TIER1 = "TIER1";
 		List<TransactionDO> transactions =jdbcTemplate.query(sql, new TransactionInfoMapper()) ;
 		return transactions;	
     }
-    
-    class TransactionInfoMapper implements RowMapper<TransactionDO> {
-  	  public TransactionDO mapRow(ResultSet rs, int arg1) throws SQLException {
-  		TransactionDO transactionData = new TransactionDO();
-  	    transactionData.setAmount(rs.getDouble("amount"));
-  	    transactionData.setTransactorUserName(rs.getString("transacterusername"));
-  	    transactionData.setTargetUserName(rs.getString("transferto"));
-  	    transactionData.setTransactionId(Integer.parseInt(rs.getString("id")));
-  	    return transactionData;
-  	  }
-  	  
-  	  
-  }
     
     @Override
     public boolean approveTransaction(int transactionId, String approverUserId, String userLevel) {
@@ -57,6 +44,55 @@ private static final String USER_ROLE_TIER1 = "TIER1";
     @Override
     public void declineTransaction(int transactionId) {
     	
+    }
+    
+    @Override
+    public List<TransactionDO> getUnapprovedNonCriticalTransactions(String userRole) {
+    	String sql = "";
+    	sql="select * from transaction where ((approverUserName is null or approverUserName='') and critical=false and transacterusername is not null) and ((status ='accepted' and transferto is not null) or (status='pending' and transferto='') or (status='pending and transferto is null'))";
+    	List<TransactionDO> transactions =jdbcTemplate.query(sql, new TransactionInfoMapper()) ;
+		return transactions;	
+    }
+    
+    class TransactionInfoMapper implements RowMapper<TransactionDO> {
+  	  public TransactionDO mapRow(ResultSet rs, int arg1) throws SQLException {
+  		TransactionDO transactionData = new TransactionDO();
+  	    transactionData.setAmount(rs.getDouble("amount"));
+  	    transactionData.setTransactorUserName(rs.getString("transacterusername"));
+  	    transactionData.setTargetUserName(rs.getString("transferto"));
+  	    transactionData.setTransactionId(Integer.parseInt(rs.getString("id")));
+  	    return transactionData;
+  	  }
+  	  
+  	  
+  }
+    
+    @Override
+    public boolean approveTransaction(int transactionId, String approverUserName) {
+    	String sql = "update transaction set approverUserName=?, status='approved' where id=?";
+    	boolean success = false;
+    	try {
+    		jdbcTemplate.update(sql, new Object[] {approverUserName, transactionId});
+    		success = true;
+    	} catch (DataAccessException e) {
+    		System.out.println("encountered Data Access Exception");
+    		throw e;
+    	}
+    	return success;
+    }
+    
+    @Override
+    public boolean declineTransaction(int transactionId, String rejecterUserName) {
+    	String sql = "update transaction set approverUserName=?, status='rejected' where id=?";
+    	boolean success = false;
+    	try {
+    		jdbcTemplate.update(sql, new Object[] {rejecterUserName, transactionId});
+        	success = true;
+    	} catch(DataAccessException e) {
+    		System.out.println("encountered Data Access Exception");
+    		throw e;
+    	}
+    	return success;
     }
     
 }
