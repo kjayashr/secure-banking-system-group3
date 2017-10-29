feb7a21b39d1e081e7c386f54abe750d8f1d7833
Sana147 <32801742+Sana147@users.noreply.github.com>
2017-10-29 10:41:27 -0700

    User.java

---
+++ User.java
@@ -0,0 +1,90 @@
+package com.ss.model;
+
+public class User {
+	
+	private String username;
+	private String firstname;
+	private String lastname;
+	private String dob;
+	private int ssn;
+	private String address;
+	private String city;
+	private String state;
+	private String country;
+	private  long contactno;
+	private String email;
+	private int postcode;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public int getSsn() {
+		return ssn;
+	}
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public long getContactno() {
+		return contactno;
+	}
+	public void setContactno(long contactno) {
+		this.contactno = contactno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(int postcode) {
+		this.postcode = postcode;
+	}
+
+}
